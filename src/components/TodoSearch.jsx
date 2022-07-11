30bcdea2d929b27f6ded25fd946fa58d2b0b79f7
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-07-11 17:34:04 -0400

    Avance hasta el video 7

---
+++ src/components/TodoSearch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/TodoSearch.css';
+
+function TodoSearch() {
+  return (
+    <input className="TodoSearch" placeholder="Cebolla" />
+  );
+}
+
+export { TodoSearch };
